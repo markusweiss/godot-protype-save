d993569fbf925258038d9ed8c552d8270effbffc
markusweiss <git@code71.de>
2023-08-29 18:18:57 +0200

    add load function

--- scripts/global.gd
+++ scripts/global.gd
@@ -19,3 +19,17 @@ func save_data():
 	var json_string = JSON.stringify(save())
 	save_game.store_line(json_string)
 	
+
+func load_data():
+	if not FileAccess.file_exists("res://savegame.dat"):
+		return
+	
+	var save_game = FileAccess.open("res://savegame.dat", FileAccess.READ)	
+	
+	while  save_game.get_position() < save_game.get_length():
+		var json_string = save_game.get_line()
+		var json = JSON.new()
+		var parse_result = json.parse(json_string)
+		var node_data = json.get_data()
+		
+		print(node_data)
--- scripts/player_data.gd
+++ scripts/player_data.gd
@@ -5,3 +5,7 @@ func _on_button_pressed():
 	Global.player_name = $playername.text
 	Global.player_id = $playerid.text
 	get_tree().change_scene_to_file("res://scenes/gaming_data.tscn")
+
+
+func _on_load_pressed():
+	Global.load_data()
