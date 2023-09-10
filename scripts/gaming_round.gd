8f8de06255490ec52b0b1efaa14f689b1cfbd350
markusweiss <git@code71.de>
2023-09-10 11:32:40 +0200

    add attack

--- scripts/gaming_round.gd
+++ scripts/gaming_round.gd
@@ -13,8 +13,10 @@ func _ready():
 		
 		$Tools.text = "\n\nDu hast Axt: " + str(Global.tools.axe) + " \nDu hast Bogen: " + str(Global.tools.bow) + " \nDu hast Picke: " + str(Global.tools.pick)
 		
-		$Defence.text = "\n\nDu Verteidigung hast: " + str(Global.worker + Global.tools["bow"] * 3 + Global.tools["axe"] * 2 + Global.tools["pick"] * 1)
+		$Defence.text = "\n\nDu hast Verteidigung: " + str(Global.worker + Global.tools["bow"] * 2 + Global.tools["axe"] * 2 + Global.tools["pick"] * 1)
 
+		$Attack.text = "\n\nDu hast Angriff: " + str(Global.worker + Global.tools["bow"] * 4 + Global.tools["axe"] * 2 + Global.tools["pick"] * 1)
+		
 func _on_save_button_pressed():
 	Global.save_data()
 	get_tree().change_scene_to_file("res://scenes/gaming_data.tscn")
